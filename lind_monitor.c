020ec53469ff08c50161ec1e88e338c15385ebd1
Benson Tsai <ben7293@gmail.com>
2014-06-16 11:55:40 -0400

    Separated some functions from lind_monitor to create lind_util
    Added lind_getcwd() in lind_util to obtain relative directory of config file

--- lind_monitor.c
+++ lind_monitor.c
@@ -782,36 +782,4 @@ int load_config() {
 	return 0;
 }
 
-char * trim(char * str) {
-	char *s1 = str, *s2 = &str[strlen(str) - 1];
 
-	while ((isspace(*s2)) && (s2 >= s1))
-		s2--;
-	*(s2 + 1) = '\0';
-
-	while ((isspace(*s1)) && (s1 < s2))
-		s1++;
-	strcpy(str, s1);
-
-	return str;
-}
-
-char *get_substring(char *string, int position, int length) {
-	char *pointer;
-	int c;
-	pointer = malloc(length + 1);
-	if (pointer == NULL) {
-		printf("Unable to allocate memory.\n");
-		exit(EXIT_FAILURE);
-	}
-
-	for (c = 0; c < position - 1; c++)
-		string++;
-
-	for (c = 0; c < length; c++) {
-		*(pointer + c) = *string;
-		string++;
-	}
-	*(pointer + c) = '\0';
-	return pointer;
-}
